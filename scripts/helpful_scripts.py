fcb589088b9f59267834849eeca9fa5cde3eaf03
Your Name <mustafatasneem53@gmail.com>
2021-11-27 20:14:00 +0530

    First Commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,24 @@
+from typing import NewType
+from brownie import FundMe,config,accounts,network,MockV3Aggregator
+from web3 import Web3
+
+
+FORKED_LOCAL_ENVIRONMENTS = ['mainnet-dev']
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development','ganache-local']
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+def get_account():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS or network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config['wallets']['from_key'])    
+def deploy_mocks():
+    print("The active network:"+ network.show_active())
+    print("Deploying Mocks...")
+    if len(MockV3Aggregator)<=0:
+        MockV3Aggregator.deploy(
+        DECIMALS,Web3.toWei(STARTING_PRICE,'ether'),{'from':get_account()}
+        )
+    print("Mocks Deployed")    
